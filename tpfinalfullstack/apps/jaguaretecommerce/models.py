ccc10bac83eeed5fccb73414a27edb2039951883
maxpipoka <martincardozo@gmail.com>
2021-06-28 20:58:57 -0300

    Agrupadas las app en una carpeta

---
+++ tpfinalfullstack/apps/jaguaretecommerce/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+
+class Categoria(models.Model):
+    nombre = CharField(max_length= 40)
+    descripcion = TextField()
+
+
+class Producto(models.Model):
+    titulo = CharField(max_length = 50)
+    imagen = TextField()
+    descripcion = TextField()
+    precio = 
+    categoria_pertenece =
+
+
+class Carrito(models.Model):
+    usuario = 
+    lista_productos = 
+    total_carrito = 
