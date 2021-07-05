7f0c57ce69e81d7a139b2f221a5c64c60ec0cc45
maxpipoka <martincardozo@gmail.com>
2021-07-05 11:21:50 -0300

    Models first modification ended

--- tpfinalfullstack/apps/jaguaretecommerce/models.py
+++ tpfinalfullstack/apps/jaguaretecommerce/models.py
@@ -3,19 +3,33 @@ from django.db import models
 # Create your models here.
 
 class Categoria(models.Model):
-    nombre = CharField(max_length= 40)
-    descripcion = TextField()
+    nombre = models.CharField(max_length= 30)
+    descripcion = models.TextField()
 
 
 class Producto(models.Model):
-    titulo = CharField(max_length = 50)
-    imagen = TextField()
-    descripcion = TextField()
-    precio = 
-    categoria_pertenece =
+    titulo = models.CharField(max_length = 40)
+    imagen = models.TextField()
+    descripcion = models.TextField()
+    precio = models.TextField()
+    categoria_pertenece = models.ForeignKey(Categoria, on_delete=models.PROTECT)
+
+
+class Usuario(models.Model):
+    email = models.EmailField()
+    nombres = models.CharField(max_length=40)
+    apellidos = models.CharField(max_length=40)
+    nombreUsuario = models.CharField(max_length=20)
+    password = models.TextField()
+    direccion = models.TextField()
 
 
 class Carrito(models.Model):
-    usuario = 
-    lista_productos = 
-    total_carrito = 
+    cliente = models.ForeignKey(Usuario, on_delete=models.PROTECT) 
+    lista_productos = models.ManyToManyField(Producto)
+    total_carrito = models.CharField(max_length=10)
+
+
+class Admin(models.Model):
+    userAdmin = models.OneToOneField(Usuario, on_delete = models.CASCADE)
+    fechaAlta = models.DateField()
