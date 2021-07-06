7bb867235e4478c55c6df45c9fc243ba2f1f2896
maxpipoka <martincardozo@gmail.com>
2021-07-06 00:50:55 -0300

    Html marckup initiated

--- tpfinalfullstack/tpfinalfullstack/settings.py
+++ tpfinalfullstack/tpfinalfullstack/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'tpfinalfullstack.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
